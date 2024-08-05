fa2199ba95b16feb88fd1bc8af30362a2be8c2e4
pensamint <belle@pensamiento.ca>
2024-08-04 20:24:27 -0400

    oops

--- inventory-mgnr/firebase.ts
+++ inventory-mgnr/firebase.ts
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "***REMOVED***",
+  apiKey: "no",
   authDomain: "inventory-mgnr.firebaseapp.com",
   projectId: "inventory-mgnr",
   storageBucket: "inventory-mgnr.appspot.com",
