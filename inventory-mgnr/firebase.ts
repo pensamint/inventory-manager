dc310e3ae6c99e2139938d17579417fe2fcebf17
pensamint <belle@pensamiento.ca>
2024-08-06 13:28:59 -0400

    updated firebase config

--- inventory-mgnr/firebase.ts
+++ inventory-mgnr/firebase.ts
@@ -4,17 +4,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "no",
-  authDomain: "inventory-mgnr.firebaseapp.com",
-  projectId: "inventory-mgnr",
-  storageBucket: "inventory-mgnr.appspot.com",
-  messagingSenderId: "400443468357",
-  appId: "1:400443468357:web:4da783a043916d4e9b912c",
-  measurementId: "G-0Z09DK252R",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
